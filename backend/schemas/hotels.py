bd9c9583461e74cd5bf10c618d2072aa9ba32e1d
123123 <1231231gmail.com>
2026-09-17 21:44:13 +0300

    create backend for page hotels

---
+++ backend/schemas/hotels.py
@@ -0,0 +1,18 @@
+from backend.schemas import Schema
+
+
+class Hotel(Schema):
+    id: int
+    title: str
+    subtitle: str
+    rating: str
+    reviewsCount: str
+    amenities: str
+    oldPrice: str = ""
+    price: str
+    category: str
+    image: str
+
+
+class HotelsPage(Schema):
+    hotels: list[Hotel]
